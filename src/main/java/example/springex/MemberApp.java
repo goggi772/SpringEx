c6ee53ee3bd3619289545090aa49071dfbd9b47a
goggi772 <goggi772@gmail.com>
2021-07-31 23:56:55 +0900

    회원 도메인 테스트

---
+++ src/main/java/example/springex/MemberApp.java
@@ -0,0 +1,19 @@
+package example.springex;
+
+import example.springex.member.Grade;
+import example.springex.member.Member;
+import example.springex.member.MemberService;
+import example.springex.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
+
