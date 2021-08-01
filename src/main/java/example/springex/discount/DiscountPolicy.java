34efca5306e06d6cdd6a820f018e13fa5b360ffc
goggi772 <goggi772@gmail.com>
2021-08-01 22:48:37 +0900

    주문과 할인 도메인

---
+++ src/main/java/example/springex/discount/DiscountPolicy.java
@@ -0,0 +1,11 @@
+package example.springex.discount;
+
+import example.springex.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
---
+++ src/main/java/example/springex/order/OrderService.java
@@ -0,0 +1,5 @@
+package example.springex.order;
+
+public interface OrderService {
+    Order createOrder(Long memberId, String itemName, int itemPrice);
+}
