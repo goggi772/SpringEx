14f4c3565d5f7aeb4e5148ad5752060eda0f2221
goggi772 <goggi772@gmail.com>
2021-08-02 23:40:03 +0900

    주문 도메인 구현체

--- src/main/java/example/springex/order/OrderServiceImpl.java
+++ src/main/java/example/springex/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package example.springex.order;
 
 import example.springex.discount.DiscountPolicy;
 import example.springex.discount.FixDiscountPolicy;
+import example.springex.discount.RateDiscountPolicy;
 import example.springex.member.Member;
 import example.springex.member.MemberRepository;
 import example.springex.member.MemoryMemberRepository;
@@ -9,7 +10,12 @@ import example.springex.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //인터페이스에만 의존하도록 변경
+    //그러나 객체를 생성하지않아 NullPointException발생
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
