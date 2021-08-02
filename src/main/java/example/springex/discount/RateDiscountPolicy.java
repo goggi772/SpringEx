572dfa95e5463094180f3ea475934325c37df6f0
goggi772 <goggi772@gmail.com>
2021-08-02 23:38:53 +0900

    정률 할인 정책 구현체

---
+++ src/main/java/example/springex/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package example.springex.discount;
+
+import example.springex.member.Grade;
+import example.springex.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
