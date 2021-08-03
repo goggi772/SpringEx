89f4b015d8b4423aa3a41309384015eecba4cd38
goggi772 <goggi772@gmail.com>
2021-08-04 00:20:51 +0900

    주문 서비스 구현체(관심사의 분리)

--- src/main/java/example/springex/order/OrderServiceImpl.java
+++ src/main/java/example/springex/order/OrderServiceImpl.java
@@ -9,13 +9,20 @@ import example.springex.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //final이 있으면 무조건 생성자를 통해 할당이 되어야 한다.
+    private final MemberRepository memberRepository;
 
     //인터페이스에만 의존하도록 변경
     //그러나 객체를 생성하지않아 NullPointException발생
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
