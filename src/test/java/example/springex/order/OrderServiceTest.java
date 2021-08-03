8aa5a84c96d5248f1caa6a182cb832c610d7fbc4
goggi772 <goggi772@gmail.com>
2021-08-04 00:22:50 +0900

    주문 도메인 테스트(관심사의 분리)

--- src/test/java/example/springex/order/OrderServiceTest.java
+++ src/test/java/example/springex/order/OrderServiceTest.java
@@ -1,16 +1,28 @@
 package example.springex.order;
 
+import example.springex.AppConfig;
 import example.springex.member.Grade;
 import example.springex.member.Member;
 import example.springex.member.MemberService;
 import example.springex.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+//    MemberService memberService = new MemberServiceImpl();
+//    OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
