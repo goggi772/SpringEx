8a5b5db770755d7b11c4268519367ae598760ceb
goggi772 <goggi772@gmail.com>
2021-08-07 00:47:37 +0900

    전체 동작 방식 구성(스프링)

--- src/main/java/example/springex/AppConfig.java
+++ src/main/java/example/springex/AppConfig.java
@@ -2,28 +2,37 @@ package example.springex;
 
 import example.springex.discount.DiscountPolicy;
 import example.springex.discount.FixDiscountPolicy;
+import example.springex.discount.RateDiscountPolicy;
 import example.springex.member.MemberService;
 import example.springex.member.MemberServiceImpl;
 import example.springex.member.MemoryMemberRepository;
 import example.springex.order.OrderService;
 import example.springex.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 
