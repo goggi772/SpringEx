8a0b2558138f242902beb4536c617288cf886098
goggi772 <goggi772@gmail.com>
2021-08-07 00:47:58 +0900

    주문 도메인 테스트(스프링)

--- src/main/java/example/springex/OrderApp.java
+++ src/main/java/example/springex/OrderApp.java
@@ -7,6 +7,8 @@ import example.springex.member.MemberServiceImpl;
 import example.springex.order.Order;
 import example.springex.order.OrderService;
 import example.springex.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
@@ -14,9 +16,13 @@ public class OrderApp {
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
