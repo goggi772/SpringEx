cd22a2698551693af75d707765e814589a15a662
goggi772 <goggi772@gmail.com>
2021-08-20 01:24:42 +0900

    컴포넌트 스캔 설정 정보 테스트

---
+++ src/test/java/example/springex/scan/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package example.springex.scan;
+
+import example.springex.AutoAppConfig;
+import example.springex.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
